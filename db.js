9dd0f94248dd3fc11bc3a5b83a2b102de127661b
Saidur289 <saidur.riaz1@gmailcomm>
2025-05-14 20:24:00 +0600

    create connetion between server and database

---
+++ db.js
@@ -0,0 +1,9 @@
+const {Pool} = require('pg')
+const pool = new Pool({
+  host: 'localhost',
+  user: 'postgres',
+  port: 5432,
+  database: "bookDB"
+ 
+})
+module.exports = pool
---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE bookDB;
+CREATE TABLE book(
+    id VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(20),
+    description VARCHAR(255)
+);
