fe242e26c9aad9532d1e9c3f12e0ad60cd4f3ed6
Saidur289 <saidur.riaz1@gmailcomm>
2025-05-14 14:51:45 +0600

    create just api without database

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`);
 })
+//Get /books => return all the books 
+app.get('/books', async(req, res) => {
+    try {
+        res.status(200).json({message: 'Books are returned'})
+        
+    } catch (error) {
+        res.json({error: error.message})
+    }
+})
+app.get('/books/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        res.status(200).json({message: 'Books  is returned', id})
+        
+    } catch (error) {
+        res.json({error: error.message})
+    }
+})
+//post /books/:id => return single book based on id
+//post /books => create a book
+//put /books/:id => update a books 
+// delete /books/:id => delete a books 
